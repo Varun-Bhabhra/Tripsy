0702f6e5dda72f2f58f4e6cd7c08796bd6f1ba2b
Varun Bhabhra <75928040+Varun-Bhabhra@users.noreply.github.com>
2021-09-21 11:04:30 +0530

    'edit'

--- src/client/js/script.js
+++ src/client/js/script.js
@@ -6,24 +6,31 @@ https://www.weatherbit.io/api/weather-forecast-16-day
 const weatherbit_base_URL = 'https://api.weatherbit.io/v2.0/forecast/daily';
 const weatherbit_api_key = '5743ac4e4b894a8eaf444a7ea695bb16';
 
+/* 
+1. Pixabay API
+https://pixabay.com/api/docs/
+*/
+const pixabay_base_URL = 'https://pixabay.com/api/';
+const pixabay_api_key = '22995546-161a66f2748e30442e887ff40';
+
 // <--ALL SELECTORS-->
 const searchBtn = document.querySelector('#searchBtn');
 const swapBtn = document.querySelector('#swap');
 const closeFlash = document.querySelector('#closeFlash');
 const flashMsg = document.querySelector('h1');
 const weatherForcast = document.querySelector('.weatherForcast');
+const searchImageTag = document.querySelector('#searchImageTag');
 let input_from = document.querySelector('#from');
 let input_to = document.querySelector('#to');
 
 // <--ALL VARIABLE DECLARITION-->
+const imageSearchTerms = ['forest', 'mountains', 'beach', 'adventure', 'streets', 'hotels', 'highways']
 
 // <--FUNCTIONS & EVENT LISTENERS-->
 
 //Flash Message Logic
 const hideFlash = () => flashMsg.classList.add('hidden');
 closeFlash.addEventListener('click', hideFlash);
-
-// Input Validation Function 
 
 // Swap Button Logic
 const InputValidation = () => {
@@ -39,25 +46,32 @@ const InputValidation = () => {
 };
 swapBtn.addEventListener('click', InputValidation)
 
-
 function test() {
     const weatherbit_final_URL = `${weatherbit_base_URL}?key=${weatherbit_api_key}&city=${input_to.value}`
+    let pixabay_final_URL = `${pixabay_base_URL}?key=${pixabay_api_key}&?q=${imageSearchTerms[Math.trunc(Math.random() * imageSearchTerms.length)]}&image_type=photo&per_page=5`;
+    console.log(pixabay_final_URL)
 
-    getForcast(weatherbit_final_URL)
+    getResponse(pixabay_final_URL)
+        .then((allData) => {
+            let searchImage = allData.hits[2].largeImageURL;
+            searchImageTag.setAttribute('src', searchImage)
+        })
+        .then(() => getData("/all"));
+
+    getResponse(weatherbit_final_URL)
         .then((allData) => {
             const { city_name, lon, lat, timezone, country_code } = allData
-            console.log(`${city_name} => ${country_code} => ${timezone} => ${Math.trunc(lon)} => ${Math.trunc(lat)}`)
+            // console.log(`${city_name} => ${country_code} => ${timezone} => ${Math.trunc(lon)} => ${Math.trunc(lat)}`)
             const { valid_date, temp } = allData.data[0];
             const { description } = allData.data[0].weather;
-            console.log(`${valid_date} => ${description} => ${temp}`);
+            // console.log(`${valid_date} => ${description} => ${temp}`);
             weatherForcast.append(`City Name = ${city_name}, Longitude = ${lon}, Latitude = ${lat}, Country Code = ${country_code}, Todays Date = ${valid_date}, Temperature = ${temp}, Climate = ${description}`)
         })
         .then(() => getData("/all"));
 }
 
 // QueryWeather Service
-async function getForcast(finalURL) {
-
+async function getResponse(finalURL) {
     const res = await fetch(finalURL);
     const weatherData = await res.json();
     return weatherData;
@@ -90,7 +104,7 @@ searchBtn.addEventListener('click', () => {
         toValue = input_to.value;
 
     if (!fromValue || !toValue) {
-        alert('Input area is empty, kindly add a city!')
+        alert('Input area is empty, add a city!')
     } else {
         test()
     }
